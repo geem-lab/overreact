d86e927b294198270293e0e091e4584841d53340
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-04 16:00:27 -0300

    Add a small Bash script for generating docs

---
+++ gendocs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pdoc overreact \
+    --docformat "numpy" \
+    --edit-url "overreact=https://github.com/geem-lab/overreact/blob/main/overreact/" \
+    --footer-text "overreact" \
+    --logo "https://raw.githubusercontent.com/geem-lab/overreact-guide/master/logo.png" \
+    --logo-link "/overreact" \
+    --math \
+    --search \
+    --show-source
+    # --output-directory docs/
