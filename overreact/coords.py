a27b8008d8301e88c3d8bc7384e8cbd9a24d896f
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2022-01-19 12:00:39 -0300

    Use less precision in some tests

--- overreact/coords.py
+++ overreact/coords.py
@@ -223,15 +223,15 @@ def _garza(
     >>> _garza(1.0, environment="benzene")
     131.
     >>> _garza(1.0, full_output=True, environment="benzene")
-    (131., 3.35, 0.23178825099342945)
+    (131., 3.35, 0.2317882509934295)
     >>> _garza(10.0, environment="benzene")
     243.
     >>> _garza(10.0, full_output=True, environment="benzene")
-    (243., 3.29, 0.49937264868206155)
+    (243., 3.29, 0.499372648682062)
     >>> _garza(100.0, environment="benzene")
     665.
     >>> _garza(100.0, full_output=True, environment="benzene")
-    (665., 1.0, 1.0758657575737383)
+    (665., 1.0, 1.07586575757374)
     """
     solvent = rx._misc._get_chemical(environment, temperature, pressure)
 
